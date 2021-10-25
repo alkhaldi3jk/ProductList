c949e3ee51e39f3616114faed7e698b117de6b83
Abdallah Alkhaldi <alkhaldi3jk@gmail.com>
2021-10-25 15:18:12 +0300

    delete and create

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
+
+const { response } = require("express");
 const express = require("express");
 const products = require ("./products")
 const app = express();
 
+//Allows our app to access the body of the request
+app.use(express.json())
 
-
-
-  app.get("/api/products", (req, res) => {
+  app.post("/api/products", (req, res) => {
+     console.log("posting",req.body)
     res.json(products);
+   
   });
 
+  app.delete("/api/products/:productId", (req, res) => {
+    console.log("deleting", req.params.productId)
+
+    const delProduct= products.find(product=> product.id === +req.params.productId)
+    res.json(delProduct);
+  
+    
+ });
 
 
 const port=8000;
